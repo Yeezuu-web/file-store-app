aa60862c69926453b09891989654ce4a0552157c
Yeezuu-web <yazugaming18@gmail.com>
2022-08-25 12:43:50 +0700

    Authenticate Frontend and Theme

---
+++ src/@core/lib/useAuth.js
@@ -0,0 +1,56 @@
+import useSWR from 'swr'
+import axios from './axios'
+import {useRouter} from 'next/router'
+import {useEffect, useState} from 'react'
+
+export default function useAuth({middleware} = {}) {
+    const router = useRouter()
+
+    const [isLoading, setIsLoading] = useState(true);
+
+    const {data: user, error, mutate} = useSWR('/user',
+        () => axios.get('/api/user').then(response => response.data.data)
+    )
+
+    const csrf = () => axios.get('/sanctum/csrf-cookie')
+
+    const login = async ({setErrors, ...props}) => {
+        setErrors([])
+
+        await csrf()
+
+        axios
+            .post('/api/login', props)
+            .then(() => mutate() && router.push('/'))
+            .catch(error => {
+                if (error.response.status != 422) throw error
+
+                setErrors(Object.values(error.response.data.errors).flat())
+            })
+    }
+
+    const logout = async () => {
+        await axios.post('/api/logout')
+
+        mutate(null)
+
+        router.push('/auth/login')
+    }
+
+    useEffect(() => {
+        if (user || error) {
+            setIsLoading(false);
+        }
+
+        if (middleware == 'guest' && user) router.push('/dashboard')
+        if (middleware == 'auth' && !user && error) router.push('/auth/login')
+    }, [user, error])
+
+    return {
+        user,
+        csrf,
+        login,
+        logout,
+        isLoading
+    }
+}
---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+import BlankLayout from "../@core/layouts/BlankLayout"
+const Home = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Home
+
+Home.getLayout = page => <BlankLayout>{page}</BlankLayout>
